c8266a5c28c7da41ea6636ba948ca67d4c521720
dodo <dodo.the.last@googlemail.com>
2010-11-19 15:38:01 +0100

    cleaner python code#

--- blain.py
+++ blain.py
@@ -2,13 +2,17 @@
 
 import sys
 
-from PyQt4 import QtCore, QtGui, uic
+from PyQt4 import QtGui, uic
 
+class Blain(QtGui.QApplication):
+    def __init__(self):
+        QtGui.QApplication.__init__(self, sys.argv)
+        self.window = uic.loadUi("qt.ui")
 
+    def run(self):
+        self.window.show()
+        sys.exit(self.exec_())
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    win = uic.loadUi("qt.ui")
-    win.show()
-    sys.exit(app.exec_())
+    Blain().run()
