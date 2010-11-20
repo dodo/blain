67d5bf3d286e99a0688b0fef7cf5363e0ddade23
dodo <dodo.the.last@googlemail.com>
2010-11-20 06:33:20 +0100

    first interactivity

--- blain.py
+++ blain.py
@@ -2,12 +2,39 @@
 
 import sys
 
-from PyQt4 import QtGui, uic
+from PyQt4 import uic, Qt as qt
 
-class Blain(QtGui.QApplication):
+
+class drug():
+    def __init__(self, **kwargs):
+        for k in kwargs:
+            setattr(self, k, kwargs[k])
+
+
+signal = drug(**dict(
+        clicked = qt.SIGNAL("clicked()")))
+
+
+
+class Slots:
+    def __init__(self, app):
+        self.app = app
+
+    def connect(self):
+        win = self.app.window
+        win.connect(win.pushButton, signal.clicked, self.test)
+
+    def test(self):
+        print "tada!"
+
+
+class Blain(qt.QApplication):
     def __init__(self):
-        QtGui.QApplication.__init__(self, sys.argv)
+        qt.QApplication.__init__(self, sys.argv)
         self.window = uic.loadUi("qt.ui")
+        #self.window.messageWidget.hide()
+        self.slots = Slots(self)
+        self.slots.connect()
 
     def run(self):
         self.window.show()
