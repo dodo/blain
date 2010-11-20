f5aefcdce60368e34fa182c832212a51ed3d22bc
dodo <dodo.the.last@googlemail.com>
2010-11-20 09:47:09 +0100

    connected lineedit, sendbutton and insert-message-into-table :D

--- blain.py
+++ blain.py
@@ -12,7 +12,10 @@ class drug():
 
 
 signal = drug(**dict(
-        clicked = qt.SIGNAL("clicked()")))
+        clicked = qt.SIGNAL("clicked()"),
+        returnPressed = qt.SIGNAL("returnPressed()"),
+        textChanged = qt.SIGNAL("textChanged(const QString&)"),
+        ))
 
 
 
@@ -22,23 +25,38 @@ class Slots:
 
     def connect(self):
         win = self.app.window
-        win.connect(win.pushButton, signal.clicked, self.test)
+        win.connect(win.sendButton, signal.clicked, self.sendMessage)
+        win.connect(win.messageEdit, signal.returnPressed, self.sendMessage)
+        win.connect(win.messageEdit, signal.textChanged, self.sendButtonController)
 
-    def test(self):
-        print "tada!"
+    def sendMessage(self):
+        txt = self.app.window.messageEdit.text()
+        if txt != "":
+            self.app.addMessage(txt)
+            self.app.window.messageEdit.setText("")
 
+    def sendButtonController(self, text):
+        self.app.window.sendButton.setEnabled( text != "" )
 
 class Blain(qt.QApplication):
     def __init__(self):
         qt.QApplication.__init__(self, sys.argv)
+        self.messages = [];
         self.window = uic.loadUi("window.ui")
-        #self.window.messageWidget.hide()
         self.slots = Slots(self)
         self.slots.connect()
 
     def run(self):
         self.window.show()
         sys.exit(self.exec_())
+
+    def addMessage(self, text):
+        mt = self.window.messageTable
+        mt.insertRow(0)
+        msg = uic.loadUi("message.ui")
+        msg.messageLabel.setText(text)
+        self.messages.append(msg)
+        mt.setCellWidget(0, 0, msg)
 
 
 if __name__ == "__main__":
