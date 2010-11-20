69899e9d7952a04b3b8d1da09c7f25fd5fdf6b43
dodo <dodo.the.last@googlemail.com>
2010-11-20 15:31:03 +0100

    add preference window

--- blain.py
+++ blain.py
@@ -15,6 +15,9 @@ signal = drug(**dict(
         clicked = qt.SIGNAL("clicked()"),
         returnPressed = qt.SIGNAL("returnPressed()"),
         textChanged = qt.SIGNAL("textChanged(const QString&)"),
+        triggered = qt.SIGNAL("triggered(bool)"),
+        hide = qt.SIGNAL("hide()"),
+        abstractbuttonclicked = qt.SIGNAL("clicked(QAbstractButton *)"),
         ))
 
 
@@ -28,6 +31,11 @@ class Slots:
         win.connect(win.sendButton, signal.clicked, self.sendMessage)
         win.connect(win.messageEdit, signal.returnPressed, self.sendMessage)
         win.connect(win.messageEdit, signal.textChanged, self.sendButtonController)
+        win.connect(win.actionQuit, signal.triggered, self.quit)
+        win.connect(win.actionPreferences, signal.triggered, self.showPreferences)
+        pref = self.app.preferences
+        pref.connect(pref, signal.hide, self.hidePreferences)
+        pref.connect(pref.buttonBox, signal.abstractbuttonclicked, self.abPref)
 
     def sendMessage(self):
         txt = self.app.window.messageEdit.text()
@@ -38,25 +46,44 @@ class Slots:
     def sendButtonController(self, text):
         self.app.window.sendButton.setEnabled( text != "" )
 
+    def quit(self, _):
+        self.app.quit()
+
+    def showPreferences(self, _):
+        self.app.window.setEnabled(False)
+        self.app.preferences.show()
+
+    def hidePreferences(self):
+        self.app.window.setEnabled(True)
+
+    def abPref(self, button):
+        print button
+        self.app.preferences.hide()
+        self.app.window.setEnabled(True)
+
+
+
 class Blain(qt.QApplication):
     def __init__(self):
         qt.QApplication.__init__(self, sys.argv)
         self.messages = [];
         self.window = uic.loadUi("window.ui")
+        self.preferences = uic.loadUi("preferences.ui")
         self.slots = Slots(self)
         self.slots.connect()
 
     def run(self):
         self.window.show()
+        self.window.statusBar.showMessage("Ready ...", 3000)
         sys.exit(self.exec_())
 
     def addMessage(self, text):
         mt = self.window.messageTable
-        mt.insertRow(0)
         msg = uic.loadUi("message.ui")
         msg.messageLabel.setText(text)
         self.messages.append(msg)
-        mt.setCellWidget(0, 0, msg)
+        i = qt.QTreeWidgetItem(mt)
+        mt.setItemWidget(i, 0, msg)
 
 
 if __name__ == "__main__":
